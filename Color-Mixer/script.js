5b72ba73eaedd04e959fc41c046577353bc01835
Thomas Brakutt <thomas.brakutt@gmail.com>
2024-11-19 22:26:17 +0100

    code description optimized at colorMixer

--- Color-Mixer/script.js
+++ Color-Mixer/script.js
@@ -2,13 +2,14 @@ const red = document.querySelector("#red"); // Zugriff auf den roten Slider
 const green = document.querySelector("#green"); // Zugriff auf den grünen Slider
 const blue = document.querySelector("#blue"); // Zugriff auf den blauen Slider
 
+// Funktion für Zuweisung der Slider zur Farbänderung
 function mainColor() {
   const colorCode = document.querySelector("p"); // Zugriff auf das "p-Element" (Anzeige oben rechts)
   const main = document.querySelector("main"); // Zugriff auf das "main-Element"
-  // Funktion für Zuweisung der Slider zur Farbänderung
-  let redSlider = red.value; // Variable für roten Slider
-  let greenSlider = green.value; // Variable für grünen Slider
-  let blueSlider = blue.value; // Variable für blauen Slider
+
+  let redSlider = red.value; // Variable für roten Slider der den Wert speichert
+  let greenSlider = green.value; // Variable für grünen Slider der den Wert speichert
+  let blueSlider = blue.value; // Variable für blauen Slider der den Wert speichert
 
   let newMainColor =
     "rgb(" + redSlider + "," + greenSlider + "," + blueSlider + ")"; // Variable zum speichern der durch die Slider veränderten Werte für aktuellen Farbcode
